3eefbe3b6ca19ff99c085627d33f05b9fd580c50
반명우 <caadiq@gmail.com>
2024-12-23 15:20:31 +0900

    BottomNavigationView 프래그먼트 연결 및 채팅 목록 row 추가

--- app/src/main/java/com/app/talkwave/view/view/MainActivity.kt
+++ app/src/main/java/com/app/talkwave/view/view/MainActivity.kt
@@ -1,14 +1,81 @@
 package com.app.talkwave.view.view
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.app.talkwave.R
 import com.app.talkwave.databinding.ActivityMainBinding
+import com.app.talkwave.viewmodel.MainFragmentType
+import com.app.talkwave.viewmodel.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            setupFragment()
+        }
+        setupBottomNavigation()
+        setupViewModel()
+    }
+
+    private fun setupFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            add(binding.containerView.id, HomeFragment(), MainFragmentType.HOME.tag)
+            add(binding.containerView.id, ChatFragment(), MainFragmentType.CHAT.tag)
+            add(binding.containerView.id, ProfileFragment(), MainFragmentType.PROFILE.tag)
+            add(binding.containerView.id, SettingsFragment(), MainFragmentType.SETTINGS.tag)
+            commit()
+        }
+
+        supportFragmentManager.executePendingTransactions()
+        supportFragmentManager.beginTransaction().apply {
+            hide(supportFragmentManager.findFragmentByTag(MainFragmentType.CHAT.tag)!!)
+            hide(supportFragmentManager.findFragmentByTag(MainFragmentType.PROFILE.tag)!!)
+            hide(supportFragmentManager.findFragmentByTag(MainFragmentType.SETTINGS.tag)!!)
+            commit()
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        binding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> mainViewModel.setCurrentFragment(0)
+                R.id.chat -> mainViewModel.setCurrentFragment(1)
+                R.id.profile -> mainViewModel.setCurrentFragment(2)
+                R.id.settings -> mainViewModel.setCurrentFragment(3)
+            }
+            true
+        }
+    }
+
+    private fun setupViewModel() {
+        mainViewModel.currentFragmentType.observe(this) { fragmentType ->
+            val currentFragment = supportFragmentManager.findFragmentByTag(fragmentType.tag)
+            supportFragmentManager.beginTransaction().apply {
+                setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+                supportFragmentManager.fragments.forEach { fragment ->
+                    if (fragment == currentFragment)
+                        show(fragment)
+                    else
+                        hide(fragment)
+                }
+            }.commit()
+
+            binding.bottomNav.selectedItemId = when (fragmentType) {
+                MainFragmentType.HOME -> R.id.home
+                MainFragmentType.CHAT -> R.id.chat
+                MainFragmentType.PROFILE -> R.id.profile
+                MainFragmentType.SETTINGS -> R.id.settings
+                else -> R.id.home
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/app/talkwave/viewmodel/MainViewModel.kt
@@ -0,0 +1,44 @@
+package com.app.talkwave.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+enum class MainFragmentType(val tag: String) {
+    HOME("home"),
+    CHAT("chat"),
+    PROFILE("profile"),
+    SETTINGS("settings")
+}
+
+@HiltViewModel
+class MainViewModel @Inject constructor() : ViewModel() {
+    private val _currentFragmentType = MutableLiveData(MainFragmentType.HOME)
+    val currentFragmentType: LiveData<MainFragmentType> = _currentFragmentType
+
+    fun setCurrentFragment(item: Int): Boolean {
+        val pageType = getPageType(item)
+        changeCurrentFragmentType(pageType)
+
+        return true
+    }
+
+    private fun getPageType(item: Int): MainFragmentType {
+        return when (item) {
+            0 -> MainFragmentType.HOME
+            1 -> MainFragmentType.CHAT
+            2 -> MainFragmentType.PROFILE
+            3 -> MainFragmentType.SETTINGS
+            else -> MainFragmentType.HOME
+        }
+    }
+
+    private fun changeCurrentFragmentType(fragmentType: MainFragmentType) {
+        if (currentFragmentType.value == fragmentType)
+            return
+
+        _currentFragmentType.value = fragmentType
+    }
+}
