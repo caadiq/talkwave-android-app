bc250156d4cabd0892a73b6425f4560dd884edb6
반명우 <caadiq@gmail.com>
2024-12-27 14:07:52 +0900

    키보드에 맞게 recyclerview가 올라가도록 수정

--- app/src/main/java/com/app/talkwave/view/view/ChatMessageActivity.kt
+++ app/src/main/java/com/app/talkwave/view/view/ChatMessageActivity.kt
@@ -14,7 +14,7 @@ class ChatMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        
+
         setupRecyclerView()
     }
 
